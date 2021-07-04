560ee09c7571b1286d0cd8fd676563df80a99fb3
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-07-04 14:30:11 +0200

    2. Change the Globally Default Flutter Font (roboto) to another Font (Quicksand)

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
       ),
       home: MyHomePage(),
     );
