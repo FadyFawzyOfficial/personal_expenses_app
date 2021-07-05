f38b8a30fc665b506c0dd506865d8e522da478df
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-07-05 16:30:01 +0200

    25. Use a Flutter Pre-Built ListTile Widget instead of our Custom Transaction List Item

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -34,49 +34,30 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (context, index) {
                 Transaction transaction = transactions[index];
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        child: Text(
-                          // Use toStringAsFixed() and the define how many decimal
-                          // places you wanna show for example 2.
-                          // No matter if you only entered one or none or if you
-                          // possibly entered a lot, then you see this gets rounded.
-                          // 9.99999 => 10.00
-                          '\$${transaction.amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 2,
-                            color: Theme.of(context).primaryColor,
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 4,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 36,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: FittedBox(
+                          child: Text(
+                            '\$${transaction.amount.toStringAsFixed(2)}',
                           ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            transaction.title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transaction.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transaction.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transaction.date),
+                    ),
                   ),
                 );
               },
