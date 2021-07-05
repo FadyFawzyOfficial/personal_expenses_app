8eb58c32175d77b3dbc95ae8dcf1b90ddabb919e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-07-05 11:04:27 +0200

    2. Loop through Lists & Generate a List of items

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues =>
+      List.generate(7, (index) {
+        final weekDay = DateTime.now().subtract(Duration(days: index));
+        var totalSum = 0.0;
+        for (var i = 0; i < recentTransactions.length; i++)
+          if (recentTransactions[i].date.day == weekDay.day &&
+              recentTransactions[i].date.month == weekDay.month &&
+              recentTransactions[i].date.year == weekDay.year)
+            totalSum += recentTransactions[i].amount;
+
+        print(DateFormat.E(weekDay));
+        print(totalSum);
+
+        return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+      });
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(16),
+      child: Row(
+        children: [],
+      ),
     );
   }
 }
