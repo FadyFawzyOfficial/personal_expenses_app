9b85a62b1a7560fa6017f29026a034562277de07
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2021-07-12 13:51:23 +0200

    2. Use the LayoutBuilder Widget to Get the maxHeight Constraints and Build Widgets with Responsive Height

--- lib/main.dart
+++ lib/main.dart
@@ -128,14 +128,14 @@ class _MyHomePageState extends State<MyHomePage> {
                           appBar.preferredSize.height -
                           // The height of the system status bar.
                           MediaQuery.of(context).padding.top) *
-                  0.4,
+                  0.3,
               child: Chart(_recentTransactions),
             ),
             Container(
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
               child: TransactionList(_userTransactions, _deleteTransaction),
             ),
           ],
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -5,50 +5,65 @@ class ChartBar extends StatelessWidget {
   final double spendingAmount;
   final double spendingPercentOfTotal;
 
-  const ChartBar(this.label, this.spendingAmount, this.spendingPercentOfTotal);
+  const ChartBar(
+    this.label,
+    this.spendingAmount,
+    this.spendingPercentOfTotal,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          // Now the text here at the top will always have a height of 24.
-          // No matter how much height it needs and therefore the text (at the top)
-          // is now aligned , the bars are aligned and the text at the bottom is alos
-          // aligned. And that of course looks better than bars jumping randomly around.
-          height: 24,
-          child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
-          ),
-        ),
-        SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) => Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(
+                '\$${spendingAmount.toStringAsFixed(0)}',
               ),
-              FractionallySizedBox(
-                heightFactor: spendingPercentOfTotal,
-                child: Container(
+            ),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    border: Border.all(color: Colors.grey),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              ),
-            ],
+                FractionallySizedBox(
+                  heightFactor: spendingPercentOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 4),
-        Container(child: Text(label)),
-      ],
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(
+                label,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
